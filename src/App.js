e71e3bb1ad7013739c794466d5cd4ca4d0cfaac0
vy-shoe <vadimulam6@gatech.edu>
2021-06-13 13:59:54 -0400

    Initial commit for html foundation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Header from "./component/header"
+import ListContainer from "./component/list-container/list-container"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
     <Header/>
+    <ListContainer/>
     </div>
   );
 }
