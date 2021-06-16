94b82589a06a7dce241929354645543db8a2546b
mna1021 <76842973+mna1021@users.noreply.github.com>
2021-06-16 16:30:21 -0400

    checkbox feature needs some work & update not yet implemented

--- src/component/list-container/list.js
+++ src/component/list-container/list.js
@@ -1,17 +1,17 @@
 import React, { Component } from "react";
-import IconButton from "@material-ui/core/IconButton";
+
 import DeleteIcon from "@material-ui/icons/Delete";
 import Checkbox from "@material-ui/core/Checkbox";
 import UpdateIcon from "@material-ui/icons/Update";
+import AddCircleIcon from "@material-ui/icons/AddCircle";
+import CheckBoxIcon from "@material-ui/icons/CheckBox";
 import "./list.css";
+import { IconButton } from "@material-ui/core";
 
 class List extends Component {
   state = {
     todos: [],
-  };
-
-  checkBox = (check) => {
-    //alert(check);
+    isCheck: false,
   };
 
   rendertodos() {
@@ -22,23 +22,21 @@ class List extends Component {
       <ul>
         {" "}
         {this.state.todos.map((todo) => (
-          <p
-            key={todo[0]}
-            style={{
-              color: "black",
-              textDecoration: this.checkBox() ? "Line-through" : null,
-            }}
-          >
+          <p key={todo[0]}>
             {" "}
-            <input
-              type="checkbox"
-              //checked={this.isChecked}
-              onChange={() => this.checkBox(todo[0])}
-              id="checkbox"
-            />
-            {todo[0]} <p1> </p1>
-            <span className="dateTime">{todo[1]} </span>
-            <IconButton id="delete">
+            <IconButton id="update" onClick={() => this.boxChecked(todo)}>
+              <Checkbox />
+            </IconButton>
+            <span
+              className={
+                this.state.isCheck ? "listComplete" : "listNotComplete"
+              }
+            >
+              <p1> </p1>
+              {todo[0]} <p1> </p1>
+              <span className="dateTime">{todo[1]} </span>
+            </span>
+            <IconButton id="update" onClick={() => this.updateItem(todo)}>
               <UpdateIcon />
             </IconButton>
             <IconButton id="delete" onClick={() => this.removeItem(todo)}>
@@ -50,6 +48,29 @@ class List extends Component {
     );
   }
 
+  render() {
+    return (
+      <span>
+        <IconButton id="add" onClick={this.increaseArr}>
+          <AddCircleIcon />
+        </IconButton>
+
+        <div className="listOutput">{this.rendertodos()}</div>
+      </span>
+    );
+  }
+
+  boxChecked = (todoID) => {
+    let booln = this.state.isCheck;
+    let updateTodo = this.state.todos.map((todo) => {
+      if (todo === todoID) {
+        return this.setState({
+          isCheck: !booln,
+        });
+      }
+    });
+  };
+
   removeItem = (todo) => {
     let arr = this.state.todos;
     return this.setState({
@@ -57,28 +78,25 @@ class List extends Component {
     });
   };
 
-  render() {
-    return (
-      <span>
-        <button className="pressButton" onClick={this.increaseArr}>
-          ADD
-        </button>
-        <div className="listOutput">{this.rendertodos()}</div>
-      </span>
-    );
-  }
+  updateItem = (todo) => {
+    let arr = this.state.todos;
+    let words = this.props.text;
+    //return alert(arr.indexOf(todo));
+    return;
+  };
 
   increaseArr = () => {
     let arr = this.state.todos;
     let words = this.props.text;
     for (let i of arr) {
-      if (i.includes(words)) return alert("No duplicates allowed");
+      if (i.includes(words)) return alert("No duplicates allowed!");
     }
+    if (words.length < 1) return alert("Please enter a list item!");
 
     this.setState({
       todos: [
+        [words, new Date().toLocaleString().replace(",", ""), true],
         ...arr,
-        [words, new Date().toLocaleString().replace(",", ""), false],
       ],
     });
   };
